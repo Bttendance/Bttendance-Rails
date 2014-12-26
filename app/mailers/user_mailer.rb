65829ab53b22b49f9fe22a4c0e901be827862957
HeeHwan PARK <heehwan.park@bttendance.com>
2014-12-26 20:24:17 +0900

    fix bug

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -14,18 +14,18 @@ class UserMailer < ActionMailer::Base
 
   def create_course(courses_user)
     @courses_user = courses_user
-    @user = User.find_by_id(params[@courses_user.user_id])
-    @course = Course.find_by_id(params[@courses_user.course_id])
-    @school = School.find_by_id(params[@course.school_id])
+    @user = User.find_by_id(@courses_user.user_id)
+    @course = Course.find_by_id(@courses_user.course_id)
+    @school = School.find_by_id(@course.school_id)
     I18n.locale = @user.locale
     mail(to: @user.email)
   end
 
   def attend_course(courses_user)
     @courses_user = courses_user
-    @user = User.find_by_id(params[@courses_user.user_id])
-    @course = Course.find_by_id(params[@courses_user.course_id])
-    @school = School.find_by_id(params[@course.school_id])
+    @user = User.find_by_id(@courses_user.user_id)
+    @course = Course.find_by_id(@courses_user.course_id)
+    @school = School.find_by_id(@course.school_id)
     I18n.locale = @user.locale
     mail(to: @user.email)
   end
