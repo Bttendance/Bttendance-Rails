aa774d36d9292d18f60c4b7f95299bea32268eb6
TheFinestArtist <contact@thefinestartist.com>
2014-12-26 14:25:02 +0900

    Devices API bug fix

--- app/api/v1/devices.rb
+++ app/api/v1/devices.rb
@@ -5,7 +5,7 @@ module V1
     resource :devices do
       desc 'Returns a list of devices, paginated'
       paginate per_page: 10
-      get '', rabl: 'devices/device' do
+      get '', rabl: 'devices/devices' do
         devices = Device.all
         @devices = paginate(Kaminari.paginate_array(devices))
       end
