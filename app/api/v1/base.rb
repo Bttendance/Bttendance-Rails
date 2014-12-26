fb5e977226f763d8f85a24d1fedb429577177f44
TheFinestArtist <contact@thefinestartist.com>
2014-12-26 14:16:18 +0900

    Added Devices List Api Back

--- app/api/v1/base.rb
+++ app/api/v1/base.rb
@@ -3,6 +3,7 @@ module V1
     version 'v1'
 
     mount V1::Users
+    mount V1::Devices
     mount V1::Schools
     mount V1::Courses
     mount V1::AttendanceAlarms
---
+++ app/views/api/devices/device.rabl
@@ -0,0 +1,3 @@
+object @device
+
+attributes :id, :platform, :uuid, :mac_address, :notification_key
