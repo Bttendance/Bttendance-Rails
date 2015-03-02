6ec15cb4e8db0917818d3e3db0ae639c058f7fd1
Devin Doolin <icddevin@gmail.com>
2015-03-02 01:50:24 -0500

    Adds 'browser' platform type to Device

--- app/models/device.rb
+++ app/models/device.rb
@@ -2,7 +2,7 @@ class Device < ActiveRecord::Base
   belongs_to :user
 
   before_validation { platform.downcase! }
-  validates :platform, presence: true, inclusion: { in: ['ios', 'android', 'xiaomi'] }
+  validates :platform, presence: true, inclusion: { in: ['ios', 'android', 'xiaomi', 'browser'] }
   validates_uniqueness_of :uuid, :allow_nil => true, :allow_blank => true
   validates_uniqueness_of :mac_address, :allow_nil => true, :allow_blank => true
 end
